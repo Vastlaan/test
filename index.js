d99517fcd202a92307fab5b21cdf08163df5ccbc
abiskana <abiskana@gmail.com>
2020-06-05 15:35:23 +0200

    added semicolon

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5555;
 
 app.listen(PORT, () => {
-    console.log(`App is listening on port ${PORT}`);
+    console.log(`App is listening on port: ${PORT}`);
 });
