2c0be3d30e74bca6c79b541ff08bf6fff1efb934
vastlaan <vastlaan@mail.com>
2020-06-06 09:41:32 +0200

    removing typo

--- index.js
+++ index.js
@@ -10,17 +10,3 @@ const PORT = process.env.PORT || 5555;
 app.listen(PORT, () => {
     console.log(`App is listening on port: ${PORT}`);
 });
-
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-    return res.status(200).json("Hello from the other side");
-});
-
-const PORT = process.env.PORT || 5555;
-
-app.listen(PORT, () => {
-    console.log(`App is listening on port ${PORT}`);
-});
-
